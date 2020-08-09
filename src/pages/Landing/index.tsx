3644384df2c734c4df7d58b21de7c9754abaf76c
Felipe Carvalho <desouzafelipecarvalho@gmail.com>
2020-08-09 10:27:22 -0300

    Estruturando e estilizando a Landing Page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 import styles from './styles'
 import landingImage from '../../images/landing.png';
+import studyIcon from '../../images/icons/study.png';
+import giveClassesIcon from '../../images/icons/give-classes.png';
+import heartIcon from '../../images/icons/heart.png';
 
 function Landing(){
     return (
         <View style={styles.container}>
-            <Image source={landingImage} />
+            <Image source={landingImage} style={styles.banner}/>
+
+            <Text style={styles.title}>
+                Seja bem-vindo, {'\n'}
+                <Text style={styles.titleBold}>
+                    O que deseja fazer?
+                </Text>
+            </Text>
+
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                    <Image source={studyIcon} />
+                    <Text style={styles.buttonText}>Estudar</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+                    <Image source={giveClassesIcon} />
+                    <Text style={styles.buttonText}>Dar aulas</Text>
+                </TouchableOpacity>
+            </View>
+            <Text style={styles.totalConnections}>
+                Total de 200 conexões já realizadas {'  '}
+                <Image source={heartIcon} />
+            </Text>
         </View>
 
     )
